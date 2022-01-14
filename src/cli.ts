9fb183e08606352a632c55934cd57a3d909a74b6
onezerobinary <>
2022-01-14 15:33:15 +0100

    added some methods to cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,40 @@
 const CFont = require("cfonts")
 const clear = require("clear")
 
+import readlineSync from "readline-sync";
+
+
+const enum Commands {
+    start = "Start the application... ",
+    stop = "Stopping the application.."
+}
+
+
+const loadCli = () => {
+    setTimeout( () => {
+        back()
+    }, 2000);
+}
+
+const back = () => {
+    if (readlineSync.keyInYN("Go back?")) {
+        cli()
+    } else {
+        process.exit();
+    }
+}
+
+const cmdStart = async () => {
+    console.log('Start called.');
+
+    loadCli();
+}
+
+const cmdStop = async () => {
+    console.log('Stop is called.');
+
+    loadCli();
+}
 
 export const cli = async () => {
     
@@ -9,8 +43,32 @@ export const cli = async () => {
     CFont.say(`Hero-CLI`, {
         align: "center",
         font: "block",
+        colors:["yellow", "#f80"]
+    })
+
+    CFont.say(`Version: 0.1 - Enrico Zanardo`, {
+        align: "center",
+        font: "console",
         colors:["yellow"]
     })
 
+    var menu = require("readline-sync"), 
+        commands = [ 
+            Commands.start,
+            Commands.stop 
+        ],
+        index = menu.keyInSelect(commands, "Commands")
+
+        switch (commands[index]) {
+            case Commands.start:
+                cmdStart();
+                break;
+            case Commands.stop:
+                cmdStop();
+                break;
+            default:
+                cli()
+                break;
+        }
 
 }
